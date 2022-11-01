fd8d07db363a132d84b72feaa7682564c07d301d
ggangliu <ggang.liu@gmail.com>
2022-11-01 06:41:48 -0700

    Added rom part

---
+++ src/rom.py
@@ -0,0 +1,42 @@
+from amaranth import *
+from math     import ceil, log2
+from amaranth.back import *
+from amaranth_soc.memory import *
+from amaranth_soc.wishbone import *
+
+from src.isa import *
+
+###############################################
+# ROM module
+###############################################
+
+class ROM(Elaboratable):
+    def __init__(self, data):
+        # Data storage
+        self.data = Memory(width=32, depth=len(data), init=data)
+        self.r = self.data.read_port()
+        self.size = len(data) * 4
+        # Initalize Wishbone bus arbiter
+        self.arb = Arbiter(addr_width=ceil(log2(self.size+1)), data_width=32)
+        self.arb.bus.memory_map = MemoryMap(addr_width=self.arb.bus.addr_width, data_width=self.arb.bus.data_width, alignment=0)
+    
+    def elaborate(self, platform):
+        m = Module()
+        #m.submodules.arb = self.arb
+        #m.submodules.r = self.r
+
+        rws = Signal(1, reset=0)
+        m.d.sync += [
+                rws.eq(self.arb.bus.cyc), 
+                self.arb.bus.ack.eq(self.arb.bus.cyc & rws)
+        ]
+
+        m.d.comb += self.r.addr.eq(self.arb.bus.adr >> 2)
+
+        with m.If((self.arb.bus.adr & 0b11) == 0b00):
+            m.d.sync += self.arb.bus.dat_r.eq(little_end(self.r.data))
+        with m.Else():
+            m.d.sync += self.arb.bus.dat_r.eq(little_end(self.r.data << ((self.arb.bus.adr&0b11) << 3)))
+
+        return m
+
---
+++ test/test_rom.py
@@ -0,0 +1,49 @@
+#####################################
+# ALU testbench
+####################################
+from amaranth.sim import Simulator 
+import sys, os
+sys.path.append(os.getcwd()+"/../")
+
+from src.isa import *
+from src.rom import *
+
+p = 0
+f = 0
+def rom_ut(dut, address, expected):
+    global p, f
+    yield dut.arb.bus.adr.eq(address)
+    yield
+    yield
+    yield
+
+    actual = yield dut.arb.bus.dat_r
+    if hexs(expected) != hexs(actual):
+        f += 1
+        print("\033[31mFAIL:\033[0m %s (got: %s)" % (hexs(expected), hexs(actual)))
+    else:
+        p += 1
+        print( "\033[32mPASS:\033[0m %s = %s" % (hexs(expected), hexs(actual)))
+
+def rom_test(dut):
+    yield #Settle()
+    print("---ROM Tests---")
+    yield dut.arb.bus.cyc.eq(1)
+    yield from rom_ut(dut, 0x0, 0x01234567)
+
+    yield #Tick()
+    print("ROM Tests: %d Passed, %d Failed" % (p, f))
+
+
+if __name__ == "__main__":
+    dut = ROM( [ 0x01234567, 0x89ABCDEF, 0x42424242, 0xDEADBEEF ] )
+	
+    def proc():
+        yield from rom_test(dut)
+	
+    sim = Simulator(dut)
+    sim.add_clock(1e-6)
+    sim.add_sync_process(proc)
+    with sim.write_vcd("test_rom.vcd"):
+        sim.run()
+
